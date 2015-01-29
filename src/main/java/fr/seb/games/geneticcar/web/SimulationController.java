a4b04c58d096505d69e30b16eedceefc607b77a1
sdescamps <sebastien.descamps@gmail.com>
2015-01-29 17:02:39 +0100

    ajout de l'equipe dans l'exception

--- src/main/java/fr/seb/games/geneticcar/web/SimulationController.java
+++ src/main/java/fr/seb/games/geneticcar/web/SimulationController.java
@@ -20,7 +20,7 @@ public class SimulationController {
 
     @RequestMapping(value="/simulation/evaluate/{team}", method = RequestMethod.POST)
     List<CarDto> evaluatePopulation(@RequestBody ArrayList<CarDto> carsDto, @PathVariable("team") Team team) {
-        validate(carsDto);
+        validate(carsDto, team);
 
         List<CarDefinition> definitions = carsDto.stream()
                 .map(carDto -> carDto.toCarDefintion())
@@ -47,85 +47,90 @@ public class SimulationController {
         return CarDto.create(team, car);
     }
 
-    private void validate(ArrayList<CarDto> carsDto) {
-        carsDto.forEach(carDto -> validate(carDto));
+    private void validate(ArrayList<CarDto> carsDto, Team team) {
+        carsDto.forEach(carDto -> validate(carDto, team));
     }
 
-    private void validate(CarDto carDto) {
-        validate(carDto.wheel2);
-        validate(carDto.wheel1);
-        validate(carDto.chassi);
+    private void validate(CarDto carDto, Team team) {
+        validate(carDto.wheel2, team);
+        validate(carDto.wheel1, team);
+        validate(carDto.chassi, team);
     }
 
-    private void validate(CarDto.Chassi chassi) {
+    // TODO ajouter le controle sur la taille des listes
+    private void validate(CarDto.Chassi chassi, Team team) {
+        if (chassi.vecteurs.size() != 16) {
+            throw new RuntimeException(team+" - le nombre de coordonnées est incorrect (!= de 16) : "+ chassi.vecteurs.size());
+        }
+
         Float[] chassiTab = chassi.vecteurs.toArray(new Float[]{});
-        validateRangeCoordChassi(Math.abs(chassiTab[0]));
-        mustBePositif(chassiTab[0]);
-        mustBeZero(chassiTab[1]);
-        validateRangeCoordChassi(Math.abs(chassiTab[2]));
-        mustBePositif(chassiTab[2]);
-        validateRangeCoordChassi(Math.abs(chassiTab[3]));
-        mustBePositif(chassiTab[3]);
-        mustBeZero(chassiTab[4]);
-        validateRangeCoordChassi(Math.abs(chassiTab[5]));
-        mustBePositif(chassiTab[5]);
-        validateRangeCoordChassi(Math.abs(chassiTab[6]));
-        mustBeNegatif(chassiTab[6]);
-        validateRangeCoordChassi(Math.abs(chassiTab[7]));
-        mustBePositif(chassiTab[7]);
-        validateRangeCoordChassi(Math.abs(chassiTab[8]));
-        mustBeNegatif(chassiTab[8]);
-        mustBeZero(chassiTab[9]);
-        validateRangeCoordChassi(Math.abs(chassiTab[10]));
-        mustBeNegatif(chassiTab[10]);
-        validateRangeCoordChassi(Math.abs(chassiTab[11]));
-        mustBeNegatif(chassiTab[11]);
-        mustBeZero(chassiTab[12]);
-        validateRangeCoordChassi(Math.abs(chassiTab[13]));
-        mustBeNegatif(chassiTab[13]);
-        validateRangeCoordChassi(Math.abs(chassiTab[14]));
-        mustBePositif(chassiTab[14]);
-        validateRangeCoordChassi(Math.abs(chassiTab[15]));
-        mustBeNegatif(chassiTab[15]);
+        validateRangeCoordChassi(Math.abs(chassiTab[0]), team);
+        mustBePositif(chassiTab[0], team);
+        mustBeZero(chassiTab[1], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[2]), team);
+        mustBePositif(chassiTab[2], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[3]), team);
+        mustBePositif(chassiTab[3], team);
+        mustBeZero(chassiTab[4], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[5]), team);
+        mustBePositif(chassiTab[5], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[6]), team);
+        mustBeNegatif(chassiTab[6], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[7]), team);
+        mustBePositif(chassiTab[7], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[8]), team);
+        mustBeNegatif(chassiTab[8], team);
+        mustBeZero(chassiTab[9], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[10]), team);
+        mustBeNegatif(chassiTab[10], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[11]), team);
+        mustBeNegatif(chassiTab[11], team);
+        mustBeZero(chassiTab[12], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[13]), team);
+        mustBeNegatif(chassiTab[13], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[14]), team);
+        mustBePositif(chassiTab[14], team);
+        validateRangeCoordChassi(Math.abs(chassiTab[15]), team);
+        mustBeNegatif(chassiTab[15], team);
 
         if (chassi.densite < 30F || chassi.densite > 300F) {
-            throw new RuntimeException("La densite du chassi n'est pas comprise entre 30 et 300 : "+chassi.densite);
+            throw new RuntimeException(team+" - la densite du chassi n'est pas comprise entre 30 et 300 : "+chassi.densite);
         }
     }
 
-    private void mustBeNegatif(Float coord) {
+    private void mustBeNegatif(Float coord, Team team) {
         if (coord > 0) {
-            throw new RuntimeException("La coordonnee doit être positive : "+ coord);
+            throw new RuntimeException(team+" - la coordonnee doit être positive : "+ coord);
         }
     }
 
-    private void mustBePositif(Float coord) {
+    private void mustBePositif(Float coord, Team team) {
         if (coord < 0) {
-            throw new RuntimeException("La coordonnee doit être positive : "+coord);
+            throw new RuntimeException(team+" - la coordonnee doit être positive : "+coord);
         }
     }
 
-    private void mustBeZero(Float coord) {
+    private void mustBeZero(Float coord, Team team) {
         if (coord != 0) {
-            throw new RuntimeException("La coordonnee doit valeur 0 : "+ coord);
+            throw new RuntimeException(team+" - la coordonnee doit valeur 0 : "+ coord);
         }
     }
 
-    private void validateRangeCoordChassi(Float coord) {
+    private void validateRangeCoordChassi(Float coord, Team team) {
         if (coord < 0.1F || coord > 1.1F) {
-            throw new RuntimeException("La coordonnee n'est pas comprise entre 0.1 et 1.1 : "+coord);
+            throw new RuntimeException(team+" - la coordonnee n'est pas comprise entre 0.1 et 1.1 : "+coord);
         }
     }
 
-    private void validate(CarDefinition.WheelDefinition wheel) {
+    private void validate(CarDefinition.WheelDefinition wheel, Team team) {
         if (wheel.radius < 0.2 || wheel.radius > 0.5) {
-            throw new RuntimeException("Le rayon doit être compris entre 0.2 et 0.5 : "+wheel.radius);
+            throw new RuntimeException(team+" - le rayon doit être compris entre 0.2 et 0.5 : "+wheel.radius);
         }
         if (wheel.density < 40 || wheel.density > 100) {
-            throw new RuntimeException("La densite doit être comprise entre 40 et 100 : "+ wheel.density);
+            throw new RuntimeException(team+" - la densite doit être comprise entre 40 et 100 : "+ wheel.density);
         }
         if (wheel.vertex < 0 || wheel.vertex > 7) {
-            throw new RuntimeException("Le sommet doit être compris entre 0 et 7 : "+ wheel.vertex);
+            throw new RuntimeException(team+" - le sommet doit être compris entre 0 et 7 : "+ wheel.vertex);
         }
 
     }
