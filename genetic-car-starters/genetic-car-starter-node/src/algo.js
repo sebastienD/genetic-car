866c1b1743afcdbdb2190d9d78ba86cdc5460456
sdescamps <sebastien.descamps@gmail.com>
2015-10-12 22:49:08 +0200

    feat: add starter node

---
+++ genetic-car-starters/genetic-car-starter-node/src/algo.js
@@ -0,0 +1,88 @@
+var http = require('http');
+var rand = require('./random.js');
+
+var team = 'RED'; //RED, YELLOW, BLUE, GREEN, ORANGE, PURPLE
+var host = 'genetic-car.herokuapp.com';
+//var host = 'localhost';
+var port = 80;
+//var port = 8080;
+
+var randomCar = function() {
+    return {
+        chassi: {
+            vecteurs: [
+                rand.randomChassisAxis(),
+                0,
+                rand.randomChassisAxis(),
+                rand.randomChassisAxis(),
+                0,
+                rand.randomChassisAxis(),
+                -1 * rand.randomChassisAxis(),
+                rand.randomChassisAxis(),
+                -1 * rand.randomChassisAxis(),
+                0,
+                -1 * rand.randomChassisAxis(),
+                -1 * rand.randomChassisAxis(),
+                0,
+                -1 * rand.randomChassisAxis(),
+                rand.randomChassisAxis(),
+                -1 * rand.randomChassisAxis()
+            ],
+            densite: rand.randomChassisDensity()
+        },
+        wheel1: {
+            radius: rand.randomWheelRadius(),
+            density: rand.randomWheelDensity(),
+            vertex: rand.randomVertex()
+        },
+        wheel2: {
+            radius: rand.randomWheelRadius(),
+            density: rand.randomWheelDensity(),
+            vertex: rand.randomVertex()
+        }
+    }
+}
+
+var cars = [];
+for (var i=0; i < 20; i++) {
+    cars.push(randomCar());
+}
+
+var jsonString = JSON.stringify(cars)
+
+var headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(jsonString, 'utf8')
+};
+
+var options = {
+    host: host,
+    port: port,
+    path: '/simulation/evaluate/' + team,
+    method: 'POST',
+    headers: headers
+};
+
+var request = http.request(options, function(res) {
+    console.log('status: ', res.statusCode);
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        if (res.statusCode == 200) {
+            var cars = JSON.parse(chunk);
+            var champion = cars.reduce(function(previous,current) {
+                return previous.score > current.score ? previous:current;
+            });
+            console.log('champion (car): ', champion.car);
+            console.log('champion (score): ', champion.score);
+        } else {
+            console.log('erreur: ', chunk);
+        }
+    });
+});
+
+request.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+});
+
+request.write(jsonString);
+request.end();
