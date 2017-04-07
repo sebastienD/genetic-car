a26a93dcf6c5878c7ccd56b3d96291378b517d72
Benoît Giraudou <bgiraudou@gmail.com>
2017-04-07 13:52:03 +0200

    Harmonize JavaScript starter (closes #23)

--- genetic-car-starters/genetic-car-starter-javascript/src/car.js
+++ genetic-car-starters/genetic-car-starter-javascript/src/car.js
@@ -6,39 +6,53 @@ import {
   randomWheelDensity,
 } from './random';
 
-export const randomCar = () => ({
+export const randomCar = () => [
+  randomChassisAxis(),
+  0,
+  randomChassisAxis(),
+  randomChassisAxis(),
+  0,
+  randomChassisAxis(),
+  -1 * randomChassisAxis(),
+  randomChassisAxis(),
+  -1 * randomChassisAxis(),
+  0,
+  -1 * randomChassisAxis(),
+  -1 * randomChassisAxis(),
+  0,
+  -1 * randomChassisAxis(),
+  randomChassisAxis(),
+  -1 * randomChassisAxis(),
+  randomChassisDensity(),
+  randomWheelRadius(),
+  randomWheelDensity(),
+  randomVertex(),
+  randomWheelRadius(),
+  randomWheelDensity(),
+  randomVertex(),
+];
+
+export const toView = car => ({
   chassis: {
-    vecteurs: [
-      randomChassisAxis(),
-      0,
-      randomChassisAxis(),
-      randomChassisAxis(),
-      0,
-      randomChassisAxis(),
-      -1 * randomChassisAxis(),
-      randomChassisAxis(),
-      -1 * randomChassisAxis(),
-      0,
-      -1 * randomChassisAxis(),
-      -1 * randomChassisAxis(),
-      0,
-      -1 * randomChassisAxis(),
-      randomChassisAxis(),
-      -1 * randomChassisAxis(),
-    ],
-    densite: randomChassisDensity(),
+    vecteurs: car.slice(0, 16),
+    densite: car[16],
   },
   wheel1: {
-    radius: randomWheelRadius(),
-    density: randomWheelDensity(),
-    vertex: randomVertex(),
+    radius: car[17],
+    density: car[18],
+    vertex: car[19],
   },
   wheel2: {
-    radius: randomWheelRadius(),
-    density: randomWheelDensity(),
-    vertex: randomVertex(),
+    radius: car[20],
+    density: car[21],
+    vertex: car[22],
   },
 });
+
+export const fromView = view => Array.of(
+  ...view.car.chassis.vecteurs, view.car.chassis.densite,
+  view.car.wheel1.radius, view.car.wheel1.density, view.car.wheel1.vertex,
+  view.car.wheel2.radius, view.car.wheel2.density, view.car.wheel2.vertex);
 
 export const fullWheelBuilder = (radius, density, vertex) => ({ radius, density, vertex });
 
